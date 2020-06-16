4f0ac9c74626d8b5672db46f252c95c77f869892
Luke Chant <luke_chant@yahoo.co.uk>
2020-06-16 19:34:46 +0100

    setup and ideas

---
+++ dank.js
@@ -0,0 +1,9 @@
+/*
+references for file upload code
+https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
+a lot of suggestions to use PHP
+ */
+
+//code to upload the photo if i dont end up using php or something else entirely
+
+//code to randomly generate a percentage of dankness and a mildly amusemening comment
