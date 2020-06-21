b8743c4efb0484c4f72023748c86b7265594247c
Luke Chant <luke_chant@yahoo.co.uk>
2020-06-21 20:05:40 +0100

    fucntioning code and styles added

--- dank.js
+++ dank.js
@@ -1,9 +1,41 @@
-/*
-references for file upload code
-https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
-a lot of suggestions to use PHP
- */
-
-//code to upload the photo if i dont end up using php or something else entirely
-
 //code to randomly generate a percentage of dankness and a mildly amusemening comment
+
+//Dank calc takes a random number ands assigns a string to a <p> using if statements
+
+var calcDank;
+var newDank;
+
+function getRandomNumber() {
+    newCalcDank = Math.floor(Math.random() * 5);
+    if (newCalcDank != calcDank) {
+        calcDank = newCalcDank;
+        howDank();
+    } else {
+        getRandomNumber();
+    }
+}
+//recursion makes the result different every time
+
+document.getElementById("danky").addEventListener("click", getRandomNumber);
+
+function howDank() {
+
+    console.log(newCalcDank);
+//console.log included for test purposes
+
+    if (calcDank === 4) {
+        document.getElementById("result").innerHTML = "Dank level is over 9000.";
+    }
+    if (calcDank === 3) {
+        document.getElementById("result").innerHTML = "Dank AF.";
+    }
+    if (calcDank === 2) {
+        document.getElementById("result").innerHTML = "Is it tho?";
+    }
+    if (calcDank === 1) {
+        document.getElementById("result").innerHTML = "Noob memes are for noobs.";
+    }
+    if (calcDank === 0) {
+        document.getElementById("result").innerHTML = "OK Boomer.";
+    }
+}
